ad6b0fba32e6df16b6c8478636c3e79a84eec8ee
Sanuja Tharinda <sanuja.18@cse.mrt.ac.lk>
2021-10-26 00:17:15 +0530

    Install libraries...

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { createTheme, ThemeProvider } from '@material-ui/core';
 import AppRouter from './Routes';
 
+const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#0C66C2"
+      },
+      secondary: {
+        main: "#FFF"
+      }
+    }
+});
+
 function App() {
+
+  
   return (
-    <AppRouter/>
+    <ThemeProvider theme = {theme}>
+      <AppRouter/>
+    </ThemeProvider>
   );
 }
 

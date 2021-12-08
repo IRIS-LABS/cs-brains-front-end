220f45856d1c7461c528674ed24dd1b9b85baafb
Sanuja Tharinda <sanuja.18@cse.mrt.ac.lk>
2021-12-08 14:26:47 +0530

    Finish header

--- src/Routes.js
+++ src/Routes.js
@@ -7,21 +7,24 @@ import Home from "./screens/home";
 import Cards from "./screens/cards";
 import CardsSearch from "./screens/cards/search";
 import Account from "./screens/account";
+import Header from "./components/header";
 
 const AppRouter = () => {
   return (
-    <Switch>
-      <Route exact path="/login" render={(props) => <Login {...props} />} />
-      <Route
-        exact
-        path="/register"
-        render={(props) => <Register {...props} />}
-      />
-      <ProtectedRoute exact path="/cards" component={Cards} />
-      <ProtectedRoute exact path="/cards/search" component={CardsSearch} />
-      <ProtectedRoute exact path="/account" component={Account} />
-      <ProtectedRoute exact path="/" component={Home} />
-    </Switch>
+    <>
+      <Header/>
+      <Switch>
+        <Route exact path="/signin" render={(props) => <Login {...props} />} />
+        <Route
+          exact
+          path="/signup"
+          render={(props) => <Register {...props} />}
+        />
+        <ProtectedRoute exact path="/cards" component={Cards} />
+        <ProtectedRoute exact path="/account" component={Account} />
+        <ProtectedRoute exact path="/" component={CardsSearch} />
+      </Switch>
+    </>
   );
 };
 
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,9 +1,232 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { alpha, makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import HomeIcon from '@material-ui/icons/Home'; 
+import PeopleIcon from '@material-ui/icons/People'; 
+import PersonAddIcon from '@material-ui/icons/PersonAdd';
+import LogoutIcon from '@material-ui/icons/ExitToApp';
+import MoreIcon from '@material-ui/icons/MoreVert';
+import ArrowRightAltIcon from '@material-ui/icons/ArrowRightAlt';
 
-const Header = () => {
-    return(
-        <div>Header</div>
-    )
-};
+const useStyles = makeStyles((theme) => ({
+  grow: {
+    flexGrow: 1,
+  },
+  link: {
+    color: theme.palette.secondary.main,
+    textDecoration: "none"
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    flexGrow: 1,
+    position: 'relative',
+    justifyContent: "center",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: "#fff",
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: theme.palette.primary.main
+  },
+  inputRoot: {
+    color: theme.palette.primary,
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    color: theme.palette.primary,
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '80%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+  navText: {
+    marginLeft: 10,
+    height: "100%",
+    fontSize: 20
+  },
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+    },
+  },
+  sectionMobile: {
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
+}));
 
-export default Header;
+export default function Header() {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const menuId = 'primary-search-account-menu';
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Cards</MenuItem>
+      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+    </Menu>
+  );
+
+  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+        <IconButton aria-label="show 4 new mails" color="inherit">
+            <HomeIcon/>
+        </IconButton>
+        <p>Profile</p>
+      </MenuItem>
+      <MenuItem>
+        <IconButton aria-label="show 11 new notifications" color="inherit">
+            <PeopleIcon/>
+        </IconButton>
+        <p>Cards</p>
+      </MenuItem>
+      <MenuItem>
+        <IconButton aria-label="show 11 new notifications" color="inherit">
+            <LogoutIcon/>
+        </IconButton>
+        <p>Sign out</p>
+      </MenuItem>
+    </Menu>
+  );
+
+  return (
+    <div className={classes.grow}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography className={classes.title} variant="h4" noWrap>
+            S
+          </Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
+          <div className={classes.sectionDesktop}>
+            <Link to = "/" className = {classes.link}>
+              <IconButton color="inherit">
+                  <HomeIcon/>
+                  <p className = {classes.navText}>Home</p>
+              </IconButton>
+            </Link>
+            <Link to = '/cards' className = {classes.link}>
+              <IconButton color="inherit" className = {classes.link}>
+                  <PeopleIcon/>
+                  <p className = {classes.navText}>Cards</p>
+              </IconButton>
+            </Link>
+            <Link to = '/signup' className = {classes.link}>
+              <IconButton color="inherit">
+                  <PersonAddIcon/>
+                  <p className = {classes.navText}>Sign up</p>
+              </IconButton>
+            </Link>
+            <Link to = '/signin' className = {classes.link}>
+              <IconButton color="inherit">
+                  <ArrowRightAltIcon/>
+                  <p className = {classes.navText}>Sign in</p>
+              </IconButton>
+            </Link>
+            <Link to = '/signout' className = {classes.link}>
+              <IconButton color="inherit">
+                  <LogoutIcon/>
+                  <p className = {classes.navText}>Sign out</p>
+              </IconButton>
+            </Link>
+          </div>
+          <div className={classes.sectionMobile}>
+            <IconButton
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+      {renderMobileMenu}
+      {renderMenu}
+    </div>
+  );
+}

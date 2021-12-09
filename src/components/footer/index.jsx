8f0b009623365c054c7caeebebf09079142b0230
DilshanSenarath <dilshan99news@gmail.com>
2021-12-09 14:43:59 +0530

    make the footer responsive

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,12 +1,16 @@
 import {
   AppBar,
   Button,
+  IconButton,
   makeStyles,
+  Menu,
+  MenuItem,
   Toolbar,
   Typography,
 } from "@material-ui/core";
 import React from "react";
-import { useHistory } from "react-router-dom";
+import { useHistory, Link } from "react-router-dom";
+import MoreIcon from "@material-ui/icons/MoreVert";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -23,11 +27,72 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
   },
+  link: {
+    color: theme.palette.primary.main,
+    textDecoration: "none",
+    padding: 10,
+  },
+  sectionDesktop: {
+    display: "none",
+    [theme.breakpoints.up("md")]: {
+      display: "flex",
+    },
+  },
+  sectionMobile: {
+    display: "flex",
+    [theme.breakpoints.up("md")]: {
+      display: "none",
+    },
+  },
 }));
 
 const Footer = () => {
   const classes = useStyles();
   const history = useHistory();
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const mobileMenuId = "footer-menu-mobile";
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+        <Link className={classes.link} to="/">
+          Home
+        </Link>
+      </MenuItem>
+      <MenuItem>
+        <Link className={classes.link} to="/cards">
+          Cards
+        </Link>
+      </MenuItem>
+      <MenuItem>
+        <Link className={classes.link} to="/signup">
+          Sign Up
+        </Link>
+      </MenuItem>
+      <MenuItem>
+        <Link className={classes.link} to="/signin">
+          Sign In
+        </Link>
+      </MenuItem>
+    </Menu>
+  );
 
   return (
     <div className={classes.root}>
@@ -36,20 +101,34 @@ const Footer = () => {
           <Typography variant="h6" className={classes.title}>
             Copyright (C) 2021 csbrains.com
           </Typography>
-          <Button onClick={() => history.push("/")} color="inherit">
-            Home
-          </Button>
-          <Button onClick={() => history.push("/cards")} color="inherit">
-            Cards
-          </Button>
-          <Button onClick={() => history.push("/signup")} color="inherit">
-            Sign Up
-          </Button>
-          <Button onClick={() => history.push("/signin")} color="inherit">
-            Sign In
-          </Button>
+          <div className={classes.sectionDesktop}>
+            <Button onClick={() => history.push("/")} color="inherit">
+              Home
+            </Button>
+            <Button onClick={() => history.push("/cards")} color="inherit">
+              Cards
+            </Button>
+            <Button onClick={() => history.push("/signup")} color="inherit">
+              Sign Up
+            </Button>
+            <Button onClick={() => history.push("/signin")} color="inherit">
+              Sign In
+            </Button>
+          </div>
+          <div className={classes.sectionMobile}>
+            <IconButton
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
+          </div>
         </Toolbar>
       </AppBar>
+      {renderMobileMenu}
     </div>
   );
 };

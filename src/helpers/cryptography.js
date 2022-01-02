a0ba3930be4bab01c64b09e56492eda06b72eaa3
DilshanSenarath <dilshan99news@gmail.com>
2022-01-03 01:55:07 +0530

    app crypto key and iv to the env file

--- src/helpers/cryptography.js
+++ src/helpers/cryptography.js
@@ -1,13 +1,9 @@
-import {
-  createCipheriv,
-  randomBytes,
-  createDecipheriv,
-} from "crypto-browserify";
+import { createCipheriv, createDecipheriv } from "crypto-browserify";
 
 class Crypto {
   constructor() {
-    this.key = randomBytes(32);
-    this.iv = randomBytes(16);
+    this.key = new Buffer.from(process.env.REACT_APP_CRYPTO_KEY, "hex");
+    this.iv = new Buffer.from(process.env.REACT_APP_CRYPTO_IV, "hex");
   }
 
   encrypt(str) {
